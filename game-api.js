a59ae9321f0b1031cfdbb3304d65e8a140a68c1d
ProCompTEAM <procompteam@gmail.com>
2018-09-23 19:00:18 +0300

    request function in game-api.js

--- game-api.js
+++ game-api.js
@@ -8,3 +8,27 @@ var api_version = function()
 	return 1;
 }
 
+/*
+	@регистрирует GET запрос на главный сервер и записывает результат через некоторое время в SERVER_RESPONCE
+	
+	@var line_params - строка в формате "параметр1=значение1+параметр2=значение2 ..";
+*/
+
+const SERVER_ADDRESS = "http://mcrpg.ru/";
+
+var SERVER_RESPONCE = "";
+var request = new XMLHttpRequest();
+
+var api_request = function(line_params)
+{	
+	SERVER_RESPONCE = "";
+	
+	request.open('GET', SERVER_ADDRESS + line_params, true);
+	
+	request.onload = function() 
+	{
+		SERVER_RESPONCE = this.responseText;
+	}
+	
+	request.send();
+}
