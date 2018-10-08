bd81ea7f574b4de8f8467757656b2b804547b1e8
DavyArt <evgenipopka@gmail.com>
2018-10-08 19:21:54 +0300

    Exit, enter room packets

---
+++ GameServer/events/AddonDisabledEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GameServer.events
+{
+	public class AddonDisabledEvent : Event
+	{
+		public addon.IModule Addon;
+		
+		public AddonDisabledEvent(addon.IModule IModule) 
+		{
+			Addon = IModule;
+		}
+		
+		public override string GetName()
+		{
+			return "Addon Disabled Event";
+		}
+		
+		public override int GetCode()
+		{
+			return Events.Code_AddonDisabledEvent;
+		}
+	}
+}
